0390f8ea349f7df65302c41c37454fd1b221d520
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2025-05-17 21:44:46 -0400

    Updates to home page

--- resources/views/livewire/website/home.blade.php
+++ resources/views/livewire/website/home.blade.php
@@ -33,25 +33,38 @@
         </div>
     </div>
 
-    <div class="px-4 sm:px-6 py-12 sm:py-24 max-w-screen-2xl mx-auto prose prose-invert">
-        <h2 class="border-l-6 border-indigo-600 ps-1.5">How We Help Businesses Thrive</h2>
-        <div class="grid grid-cols-2 gap-6">
-            <div class="col-span-2 sm:col-span-1">
-                <ul class="space-y-4 text-lg leading-relaxed list-disc pl-6">
-                    <li><strong>Focus on growth —</strong> we manage your IT so you can run your business without distractions.</li>
-                    <li><strong>Make smarter decisions —</strong> our custom dashboards turn your data into real-time insights.</li>
-                    <li><strong>Streamline your workflows —</strong> with software built around <strong>you</strong>, not the other way around.</li>
-                    <li><strong>Stop tech chaos —</strong> we unify your infrastructure, support, and systems into one smooth operation.</li>
-                </ul>
-            </div>
-            <div class="col-span-2 sm:col-span-1">
-                
-            </div>
+    <div class="px-4 sm:px-6 py-12 sm:py-24 max-w-screen-2xl mx-auto w-full prose prose-invert text-center">
+        <h2>Trusted by South Florida Professionals</h2>
+        <div class="grid justify-center grid-cols-2 gap-8 text-zinc-400 sm:gap-12 md:grid-cols-3">
+            <a href="#" class="flex justify-center items-center">
+                <img src="{{ asset('logos/mjacctaxdotcom.png') }}" alt="MJ Accounting Logo" class="h-20 grayscale hover:grayscale-0 transition-all">
+            </a>
+            <a href="#" class="flex justify-center items-center">
+                <img src="{{ asset('logos/sosmetaxservices.svg') }}" alt="SosME Logo" class="h-20 grayscale hover:grayscale-0 transition-all">
+            </a>
+            <a href="#" class="flex justify-center items-center">
+                <img src="{{ asset('logos/tpbs.webp') }}" alt="TPBS Logo" class="h-20 grayscale hover:grayscale-0 transition-all opacity-75 hover:opacity-100">
+            </a>
+            <a href="#" class="flex justify-center items-center">
+                <img src="{{ asset('logos/atease.webp') }}" alt="At Ease Logo" class="h-20 grayscale hover:grayscale-0 transition-all">
+            </a>
+            <a href="#" class="flex justify-center items-center">
+                <img src="{{ asset('logos/equitable.png') }}" alt="At Ease Logo" class="h-20 grayscale hover:grayscale-0 transition-all">
+            </a>
+            <a href="#" class="flex justify-center items-center">
+                <img src="{{ asset('logos/mjd.png') }}" alt="At Ease Logo" class="h-20 grayscale hover:grayscale-0 transition-all">
+            </a>
         </div>
     </div>
 
     <div class="px-4 sm:px-6 py-12 sm:py-24 max-w-screen-2xl mx-auto prose prose-invert">
-        <h2 class="border-l-6 border-indigo-600 ps-1.5">What we deliver</h2>
+        <p class="text-center">
+            {{ __("You’ve got a business to run — and the last thing you need is unreliable tech, scattered systems, or slow support. At Codewize, we help growing companies turn technology into an advantage. Whether you're struggling with aging infrastructure, overwhelmed by patchwork software, or simply tired of IT that holds you back, we step in with modern, maintainable solutions. From secure cloud setups to custom-built dashboards and business apps, we design systems that work the way *you* work — faster, safer, and smarter. It's not just support. It's strategy. Let’s simplify your tech, so you can scale your business with confidence.") }}
+        </p>
+    </div>
+
+    <div class="px-4 sm:px-6 py-12 sm:py-24 max-w-screen-2xl mx-auto prose prose-invert">
+        <h2>What we deliver</h2>
         <div class="grid grid-cols-3 gap-6">
             <div class="col-span-3 sm:col-span-1">
                 <h3>Modern IT Solutions</h3>
