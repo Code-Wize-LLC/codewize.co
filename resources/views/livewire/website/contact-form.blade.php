2b387386499cd164f4af02073c89fedd17e2fba9
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2025-05-20 19:00:57 -0400

    ..

--- resources/views/livewire/website/contact-form.blade.php
+++ resources/views/livewire/website/contact-form.blade.php
@@ -1,6 +1,6 @@
 <div>
-    <form wire:submit.prevent="save" wire:recaptcha>
-        {{-- @csrf --}}
+    <form wire:submit.prevent="save">
+        @csrf
         <div class="flex flex-col gap-6">
             <div class="rounded-xl border bg-white dark:bg-stone-950 dark:border-stone-800 text-stone-800 shadow-xs">
                 <div class="px-4 sm:px-10 py-6 sm:py-8 space-y-6 text-left">
