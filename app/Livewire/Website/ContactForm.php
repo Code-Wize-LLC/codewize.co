af8891e29931efa9c6aa2ad9af666f6a7e804dfc
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2025-05-20 19:05:03 -0400

    ..

--- app/Livewire/Website/ContactForm.php
+++ app/Livewire/Website/ContactForm.php
@@ -41,31 +41,27 @@ class ContactForm extends Component
             'details' => ['nullable', 'max:500'],
         ]);
 
-        dd('Hello');
-
         // Save to DB
 
         // Post to Hubspot
-        defer(function () {
-            $hs = new HubspotLeadService;
+        $hs = new HubspotLeadService;
 
-            $payload = [
-                'email' => $this->email,
-                'firstname' => $this->firstname,
-                'lastname' => $this->lastname,
-                'phone' => $this->phone,
-                'topic' => $this->topic,
-                'details' => $this->details,
-            ];
+        $payload = [
+            'email' => $this->email,
+            'firstname' => $this->firstname,
+            'lastname' => $this->lastname,
+            'phone' => $this->phone,
+            'topic' => $this->topic,
+            'details' => $this->details,
+        ];
 
-            if (! $hs->findLeadByEmail($this->email)) {
-                try {
-                    $hs->createLead($payload);
-                } catch (\Throwable $th) {
-                    Log::error($th);
-                }
+        if (! $hs->findLeadByEmail($this->email)) {
+            try {
+                $hs->createLead($payload);
+            } catch (\Throwable $th) {
+                Log::error($th);
             }
-        });
+        }
 
         session()->flash('success', 'Thank for reaching out. One of our agents will get in touch with you soon.');
         $this->redirect(url: route('contact'), navigate: false);
