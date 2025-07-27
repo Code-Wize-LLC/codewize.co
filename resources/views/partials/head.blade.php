5bedb1a4887dba091e92379dcf171ec73df2158e
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2025-07-27 08:04:02 -0400

    set canonical url

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,9 +10,12 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=inter:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
 
-<!-- Fathom - beautiful, simple website analytics -->
-<script src="https://cdn.usefathom.com/script.js" data-site="USINGNHJ" defer></script>
-<!-- / Fathom -->
+@guest
+    <link rel="canonical" href="{{ route('home') }}" />
+    <!-- Fathom - beautiful, simple website analytics -->
+    <script src="https://cdn.usefathom.com/script.js" data-site="USINGNHJ" defer></script>
+    <!-- / Fathom -->
+@endguest
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
